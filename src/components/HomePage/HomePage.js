e1e650ad407a37cf7c99ca72f619461d5a1669a5
Williamlin308 <williamlin330088@gmail.com>
2021-12-28 01:00:57 +0800

    Complete all layouts of main sections in home page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,6 +3,7 @@ import Header from '../Header/Header';
 import ConferenceSection from './ConferenceSection';
 import ContentSection from './ContentSection';
 import section from '../../lists/section';
+import popupList from '../../lists/popupList';
 
 export default function HomePage() {
   return (
@@ -14,6 +15,23 @@ export default function HomePage() {
       {section.map((s) => {
         return <ContentSection title={s.title} info={s.info} key={s.title} />;
       })}
+      <section className="px-[20px] mt-[30px] bg-divider bg-repeat-x bg-center bg-dividerH">
+        <ul className="sm:flex sm:justify-around max-w-[1150px] m-auto">
+          {popupList.map((p) => {
+            return (
+              <li className="mb-[30px] sm:w-[47%]">
+                <h4 className="text-center text-grey mt-[30px] mb-[10px] text-[11px]">
+                  {p.subtitle}
+                </h4>
+                <h2 className="text-center text-black mb-[15px] text-[18px]">
+                  {p.title}
+                </h2>
+                <p className="text-dark mb-[20px] text-[13px]">{p.content}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
     </>
   );
 }
---
+++ src/lists/popupList.js
@@ -0,0 +1,16 @@
+const popupList = [
+  {
+    subtitle: 'POP-UP SHOP',
+    title: 'Draft Supply Co.',
+    content:
+      'New to Build in 2012 is the Draft Supply Co. pop-up shop, stocking quality goods for the discerning digital craftsman. Low on stationary, need a gift, or feel like picking up some Build swag? We’re open for business.',
+  },
+  {
+    subtitle: 'POP-UP CAFE',
+    title: 'The Black Box',
+    content:
+      'We’ll be establishing a base of operations at The Black Box on Hill Street, right at the epicentre of the Build festivities. Our pop-up cafe will serve a full menu all day, offer free wifi, and will be staffed by Build volunteers who’ll be constantly on hand to answer any questions.',
+  },
+];
+
+export default popupList;
